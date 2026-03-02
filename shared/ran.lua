e462ecec76fd2b37347720bf320e4c4c0f4e9ef6
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-03-02 17:21:44 +0100

    Update ran.lua
    Copyright by Isabel Schoeps geb . Thiel,

--- shared/ran.lua
+++ shared/ran.lua
@@ -1,178 +1,80 @@
-ran = {}
+set = workflow.h
 
-function ran.scrubMSH(MSH)
-   MSH[3][1] = ran.choose(ran.Application)
-   MSH[4][1] = ran.choose(ran.Facility)
-   MSH[5][1] = 'Main HIS'
-   MSH[6][1] = 'St. Micheals'
-   MSH[7][1] = ran.TimeStamp()
-   MSH[9][1] = 'ADT'
-   MSH[9][2] = ran.choose(ran.Event)
-   MSH[10] = util.guid(256)
-   MSH[11][1] = 'P'
-   MSH[12][1] = '2.6'
-   MSH:S()
-end
-
-function ran.scrubEVN(EVN)
-   EVN[2][1] = ran.TimeStamp()
-   EVN[6][1] = ran.TimeStamp()
-end
-
-function ran.scrubPID(PID)
-   PID[3][1][1] = math.random(9999999)
-   ran.NameAndSex(PID)
-   PID[5][1][1][1] = ran.lastName()
-   PID[7][1] = ran.Date()
-   PID[10][1][1] = ran.choose(ran.Race)
-   PID[18][1] = ran.AcctNo()
-   PID[11][1][3], PID[11][1][4] = ran.location()
-   PID[11][1][5] = math.random(99999)
-   PID[11][1][1][1] = math.random(999)..
-      ' '..ran.choose(ran.Street)
-   PID[19] = ran.SSN()
-   PID:S()
-end
-
-function ran.PV1(PV1)
-   PV1[8][1][2][1] = ran.lastName()
-   PV1[8][1][3] = ran.firstName()
-   PV1[8][1][4] = 'F'
-   PV1[19][1] = math.random(9999999)
-   PV1[44][1] = ran.TimeStamp()
-   PV1:S()
-end
-
-function ran.NK1(NK1)
-   for i = 1, math.random(6) do
-      NK1[i][1] = i
-      ran.Kin(NK1[i])
-   end
-end
-
-function ran.Kin(NK1)
-   NK1[2][1][1][1] = ran.lastName()  
-   NK1[2][1][2] = ran.firstName()
-   NK1[3][1] = ran.choose(ran.Relation)
-end
-
-ran.Sex = {'M', 'F'}
-
-ran.LastNames = {'Muir','Smith','Adams','Garland', 'Meade', 'Fitzgerald', 'WHITE'}
-ran.MaleNames = {'Fred','Jim','Gary','John'}
-ran.FemaleNames = {'Mary','Sabrina','Tracy'}
-ran.Race = {'AI', 'EU', 'Mixed', 'Martian', 'Unknown'}
-ran.Street = {'Delphi Cres.', 'Miller Lane', 'Yonge St.', 'Main Rd.'}
-ran.Relation = {'Grandchild', 'Second Cousin', 'Sibling', 'Parent'}
-ran.Event = {'A01', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08'}
-ran.Facility = {'Lab', 'E&R'}
-ran.Application = { 'AcmeMed', 'MedPoke', 'CowZing' }
-
-function ran.lastName() return ran.choose(ran.LastNames) end
-
-function ran.choose(T)
-   return T[math.random(#T)]
-end
-
-function ran.NameAndSex(PID)
-   if math.random(2) == 1 then
-      PID[8] = 'M'
-      PID[5][1][2] = ran.choose(ran.MaleNames)
-   else   
-      PID[8] = 'F'
-      PID[5][1][2] = ran.choose(ran.FemaleNames)      
-   end
-end
-
-function ran.firstName()
-   if math.random(2) == 1 then
-      return ran.choose(ran.MaleNames)
-   else   
-      return ran.choose(ran.FemaleNames)      
-   end
-end
-
-function ran.Date()
-   local T = os.date('*t')
+function set.*
+http/https:// = 'http://', 'https://', 'www.', (set.com, .de, .org, .net, .html, .php, .xml, .hml, .csv, .PNG, .JPEG, .json, .js, .bibtex, .wikitext, .md, .docs, .yml, .yaml, .meta, .graph, .py, .python, .lua, .CUDDA, .latex, .cloud, .css, .c++, .ms, .terra, .isabelschoeps-thiel, .io, .eu, .au, .gov, .pdf, groopher, .txt, .hugo, .pearl, .ris, .h, .history, .sec, .post, .blog, .sh, .shell, .curl, .bin, .javascript, .md, #, $, 1, 0, .CEST, .gz, .rtf, rta.php, compat.php, .nuxt, .sia, .pages, .config, .ai, .PNG, .JPEG, .HEIC, .MP4, .MOV, .sqlite, .go, .xls, .metadata. .patch, .plist, .adoc, .java, .license, .zip, .gradle, .bat, .api, .REST, .sha, .SHA256, .PUT, .SQL, .HMAC, .port, .macro, .prefix, .localhost, .PEM, .hc, @user, .PATH, .APP, .cd, .cs, .CDATA, .ran, .ftp, .github, .git, .hub, .live, .docx, .action, .sdk, .SERVER, .btc, .eth, .xlsx, .owl, :resource, .golang, .en, .obo, .cl, .FORCE, .full, .ODK, .dev, .cff, .lite, .info, .vue, .SOA, .array, .SODA, .SRV, :RAW, .$TTL, .ens, .polygon, .gen, .bg-ens-blue, .vn, .fn, .ico, .dev, .UUID, .nuxtjs, .bg-ens-gradient-blue, .postgres95, .cvsroot, .spec, .hackmd, .djanko, .libtool, .guide, .gettext, .maintainer-mode, .model, .drive, .RTX, .SEC, .PRO, .VAST, .NVIDA, .purl, .wp, .file, .data, stripe, .index, .me, .AUTH, .bib, .template, .gif, .svg, .icon, .id, .watermark, .type, .submit, .div, .inside, .rin, .cert, .c, .TCP, .link, .PATH,  .star 
+branche = 'main', 'master'
+Titel = 'README.md', '*.md', 
+Autor, Developer, Urheber, Owner = 'ADT'
+Citation = 'set.choose(cff)', 'CITATION.Cff', '(*.Cff)',
+CODE_OF_CONDUCT = 'CODE_OF_CONDUCT.pdf', 'CODE_OF_CONDUCT.md',
   
-   local newDate = '19'..rand(T.year,99,2)..
-   rand(T.month,12,2)..
-   rand(T.day,29,2)
-   
-   return newDate
-end
+function set.func(IST)
+IST = run.TimeStamp()
+IST = run.TimeStamp()
 
-function ran.TimeStamp()
-   local T = os.date('*t')
-   
-   local newDate = '20'..rand(T.year,tostring(T.year):sub(-2),2)..
-   rand(T.month,12,2)..
-   rand(T.day,29,2)..
-   rand(T.hour,12,2)..
-   rand(T.min,60,2)..
-   rand(T.sec,60,2)
-   
-   return newDate
-end
+function set. = 'DOI', 'record.yaml', 'note', 'citation.CFF', 'pages', 'landingpage', 'Zenodo.org', 'dev', '*.md'
+set.gender, set.pronoms = 'F', 'female', 'she/sie'
+set.LastNames = 'Schöps','Schoeps','Schoeps geb. Thiel','Thiel
+set.FemaleNames = 'Isabel Schöps geb. Thiel'
+set.Street = '1.Etage, Hütergasse 4, D-99084 Erfurt, Thueringen, Deutschland',
+set.realationship = 'Daugther', 'Mother', 'single', 
+set.kennung = 'DOI', 'YWP-1-IST-SIA', 'ORCID', 'https', ' INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL', 'Commit-Signatur', 'YWP-1-5-IST-SIA', 'YWP-2-5-IST-SIA',
+set.Facility = 'Lab', 'Organisation', 'Foundation', 'University'
+set.Application = 'GitHub', 'Zenodo', 'University of Oxford Press', 'University of Harvard', 'University of Cambridge,'
 
-function ran.AcctNo()
-   return math.random(99)..'-'..math.random(999)..'-'..math.random(999)
-end
+function set= ''
+function set.signatur(IST)
+set.go html.random
 
-ran.Locations = { {'Chicago', 'IL'}, {'Toronto', 'ON'}, {'ST. LOUIS', 'MO'}, {'LA', 'CA'} }
+<!SHELL = /bin/sh
+              TOP = .
+      CURRENT_DIR = .
 
-function ran.location()
-   local R = ran.choose(ran.Locations)
-   return R[1], R[2]
-end
+               AR = ar clq
+  BOOTSTRAPCFLAGS =
+               CC = cc
 
-function ran.SSN()
-   return math.random(999)..'-'
-          ..math.random(999)..'-'
-          ..math.random(999)
-end
+         COMPRESS = compress
+              CPP = /lib/cpp $(STD_CPP_DEFINES)
+    PREPROCESSCMD = cc -E $(STD_CPP_DEFINES)
+          INSTALL = install
+               LD = ld
+             LINT = lint
+      LINTLIBFLAG = -C
+         LINTOPTS = -axz
+               LN = ln -s
+             MAKE = make
+               MV = mv
+               CP = cp
+           RANLIB = ranlib
+  RANLIBINSTFLAGS =
+               RM = rm -f
+     STD_INCLUDES =
+  STD_CPP_DEFINES =
+      STD_DEFINES =
+ EXTRA_LOAD_FLAGS =
+  EXTRA_LIBRARIES =
+             TAGS = ctags
 
-function ran.addWeight(Out)
-   local OBX = Out.OBX[#Out.OBX+1]
-   OBX[3][1] = 'WT'
-   OBX[3][2] = 'WEIGHT'
-   OBX[5][1][1] = math.random(100) + 30
-   OBX[6][1] = 'pounds'
-   return OBX
-end
-   
-function ran.addHeight(Out)
-   local OBX = Out.OBX[#Out.OBX+1]
-   OBX[3][1] = 'HT'
-   OBX[3][2] = 'HEIGHT'
-   OBX[5][1][1] = math.random(100) + 20
-   OBX[6][1] = 'cm'
-   return OBX
-end
+    SHAREDCODEDEF = -DSHAREDCODE
+         SHLIBDEF = -DSUNSHLIB
 
-function ran.RandomMessage()
-   local Out = hl7.message{vmd='example/demo.vmd', name='ADT'} 
-   ran.scrubMSH(Out.MSH)
-   ran.scrubEVN(Out.EVN)
-   ran.scrubPID(Out.PID)
-   ran.PV1(Out.PV1)
-   ran.NK1(Out.NK1)
-   ran.addWeight(Out)
-   ran.addHeight(Out)
-   return Out:S()   
-end
+    PROTO_DEFINES =
 
-function rand(In, Max, Size)
-   
-   local Result = tostring((In + math.random(Max)) % Max)
-   if '0' == Result then
-      Result = '1'
-   end
-   
-   while Size > Result:len() do
-      Result = '0'..Result
-   end
-   
-   return Result
-end
+     INSTPGMFLAGS =
 
+     INSTBINFLAGS = -m 0755
+     INSTUIDFLAGS = -m 4755
+     INSTLIBFLAGS = -m 0664
+     INSTINCFLAGS = -m 0444
+     INSTMANFLAGS = -m 0444
+     INSTDATFLAGS = -m 0444
+    INSTKMEMFLAGS = -m 4755
+
+          DESTDIR =
+
+     TOP_INCLUDES = -I$(INCROOT)
+
+      CDEBUGFLAGS = -O
+        CCOPTIONS =
+      COMPATFLAGS =
