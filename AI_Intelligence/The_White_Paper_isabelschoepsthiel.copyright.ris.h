735c1659207df779ad672393b9e2aeb0b292e9c5
Isabel Schöps (Thiel) <u5983574840@gmail.com>
2026-04-02 18:10:29 +0200

    The Yellow Whitepaper
    Autorin, Urheberin; Deepweb-Forscherin, Ich Frau Isabel Schöps geb. Thiel Zeitstempel der Eintragung: Donnerstag den, 2026-04-02, 18:09:00 Uhr, Mitteleuropäische, Ort, Wohnung: Isabel Schöps (Thiel), 1.Etage, Hütergasse 4, D-99084 Erfurt, Thüringen, Deutschland. Gutachten: SIA Security Intelligence Artefact, internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL, und The Yellow Whitepaper YWP-1-IST-SIA

---
+++ AI_Intelligence/The_White_Paper_isabelschoepsthiel.copyright.ris.h
@@ -0,0 +1,12 @@
+TY  - BOOK
+T1  - The White Paper
+A1  - Nakamoto, S.
+A1  - Bridle, J.
+A1  - Brekke, J.K.
+A1  - Vickers, B.
+SN  - 9781999675929
+T3  - G - Reference,Information and Interdisciplinary Subjects Series
+UR  - https://books.google.de/books?id=5BFDvAEACAAJ
+Y1  - 2019
+PB  - Ignota Books
+ER  - 
---
+++ AI_Intelligence/The_White_Paper_isabelschoepsthiel_copyright.enw.h
@@ -0,0 +1,10 @@
+%0 Book
+%T The White Paper
+%A Nakamoto, S.
+%A Bridle, J.
+%A Brekke, J.K.
+%A Vickers, B.
+%@ 9781999675929
+%U https://books.google.de/books?id=5BFDvAEACAAJ
+%D 2019
+%I Ignota Books
